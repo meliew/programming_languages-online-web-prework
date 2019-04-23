d2719bc03782cc270a60b8f8e6c1d546a3e31979
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-23 19:13:43 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
-    new_hash[language] = {style: style}
+    new_hash[language] = style: style
     new_hash[language][:type]
 
 end
