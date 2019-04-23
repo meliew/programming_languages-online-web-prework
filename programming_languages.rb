02d34bfee3447e3e651073c4d936f6fa9d67111e
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-23 19:57:23 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, names|
+    names.each do |name, description|
+      new_hash[name] ||= {style: []}
+      new_hash[name][:type] = description[:type]
+      new_hash[name][:style] << style
+    end
+  end
+  new_hash
 end
+
+
+#iterate over the language hash
+#create a new hash that has the language as a key, and value will be another hash with type and style.
