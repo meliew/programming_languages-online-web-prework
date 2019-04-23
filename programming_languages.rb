2910429f439c0633c2107ea17f117f32493f4ef5
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-23 19:12:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,13 @@
 
-  def reformat_languages(languages)
-    new_hash = {}
-    languages.each do |style, language|
-      new_hash[language] = {style: style, type: type}
+def reformat_languages(languages)
+  new_hash = {}
+  languages.each do |style, language|
+    new_hash[language] = {style: style}
+    new_hash[language][:type]
 
-  end
-  new_hash
-  end
+end
+new_hash
+end
 
 
 #iterate over the language hash
